133faf8ad3fce91b152582590eb0aea3539aafcf
Rosa, Genesis <mrgenesis@hotmail.com>
2020-08-01 16:34:13 -0400

    Finalizado projeto pararecido com o curso

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ bot.on('message', async function (msg) {
     const chatId = msg.chat.id; // recebe a id do chat
     console.log(msg.text); // print mensaem no console
 
+    
     const dfResponse = await dialogflow.sendMessage(chatId.toString(), msg.text);
-    bot.sendMessage(chatId, dfResponse.text);
+    
+    let responseText = dfResponse.text;
+
+    if (dfResponse.intent === 'busca vídeos sobre nodejs') {
+        responseText = await youtube.searchVideos(responseText, dfResponse.fields.linguage.stringValue);
+    }
+    
+    bot.sendMessage(chatId, responseText);
 });
